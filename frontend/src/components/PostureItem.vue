a40c5c48bb682eb4b4f6c1a68d97e6ca10efb5ac
이연지 <yeonji1012@naver.com>
2021-10-25 18:06:29 +0900

    S05P31C201-69 feat: [FE] 자세교정 운동 아이템 컴포넌트

---
+++ frontend/src/components/PostureItem.vue
@@ -0,0 +1,32 @@
+// 자세교정 선택 아이템 1개
+<template>
+  <div>
+    <div class="pi-div">
+      <h1>스쿼트</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostureItem',
+  data (){
+    return {
+
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/common.scss";
+
+.pi-div {
+  background-color: $light-color;
+  border-radius: 20px;
+  width: 220px;
+  height: 290px;
+}
+
+</style>
--- frontend/src/views/Posture/PostureLists.vue
+++ frontend/src/views/Posture/PostureLists.vue
@@ -1,18 +1,33 @@
 // 자세교정 선택 페이지
 <template>
   <div>
+    <h1>
+      부족한 운동을 선택하고<br>집중적으로 교정해보세요
+    </h1>
 
+    <PostureItem />
   
   </div>
 </template>
 
 <script>
+import PostureItem from "@/components/PostureItem.vue"
+
 export default {
   name: 'PostureLists',
+  components: {
+    PostureItem,
+  },
 
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import "@/styles/common.scss";
+
+h1 {
+  margin-top: 30px;
+  margin-bottom: 70px;
+}
 
 </style>
