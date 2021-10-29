22e4492133bbb34274b6599feac9c436085f8869
박세영 <seyoungs22@naver.com>
2021-10-29 15:32:20 +0900

    [S05P31C201-76 ] feat: count 그래프 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,16 @@ import router from './router'
 import store from './vuex/store'
 import axios from 'axios'
 import AxiosPlugin from 'vue-axios-cors';
+import VueApexCharts from 'vue-apexcharts'
 
 Vue.use(AxiosPlugin);
 axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded'
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 Vue.config.productionTip = false
+Vue.use(VueApexCharts)
 
+Vue.component('apexchart',VueApexCharts)
 new Vue({
   router,
   store,
--- frontend/src/views/Course/CourseDetail.vue
+++ frontend/src/views/Course/CourseDetail.vue
@@ -16,7 +16,10 @@
       </div> -->
       <!-- 운동별로 정해진 반복 세트(set)와 횟수(count) 들어옴 -->
       <div class="set-count">
-        <div>{{set}}세트  {{count}}회</div>
+        <div class="cont">{{set}}0세트  {{count}}0회</div>
+        <div id="chart">
+        <apexchart type="radialBar" height="150" :options="chart.chartOptions" :series="chart.series"></apexchart>
+        </div>    
       </div>   
 
       <div class="course-content-step">        
@@ -33,18 +36,38 @@
 import Vedio from '@/components/Detail/Vedio.vue'
 import Exe from '@/components/Detail/Exe.vue'
 import Clock from '../../components/Detail/Clock.vue'
+import VueApexCharts from 'vue-apexcharts'
 export default {
   name: 'Detail',
   components:{
     Vedio,
     Exe: Exe,
     Clock,
+    apexchart:VueApexCharts,
   },
   data(){
     return{
       items:['1','2','3'],
       msg:"시작",
+      chart: {
+        series: [70],
+        chartOptions: {
+        chart: {
+          height: 150,
+          type: 'radialBar',
+        },
+        plotOptions: {
+          radialBar: {
+              hollow: {
+              size: '70%',
+            }
+          },
+        },
+        labels: ['count'],
+        },      
     }
+  }
+    
   },
   methods: {
     start(){
@@ -74,21 +97,21 @@ export default {
 }
 .comment {
   top: 18%;
-  left: 36%;
+  left: 40%;
   position: absolute;  
   font-size: 24px;
 }
 .course-vedio {
   width: 34%;
   height: 64%;  
-  top: 24%;
+  top: 26%;
   left: 13%;
   position: absolute;
   border: solid black;
   margin: 10px;
 }
 img {
-    top: 42%;
+    top: 44%;
     width: 20%;
     height: 42%;  
     left: 50%;
@@ -97,7 +120,7 @@ img {
     margin: 10px;
 }
 .clock {
-  top: 26%;
+  top: 28%;
   right: 35%;
   position: absolute;  
 }
@@ -109,12 +132,20 @@ img {
 }
 .set-count {
   top: 26%;
-  right: 20%;
+  right: 21%;
+  width: 150px;
   position: absolute;  
   font-size: 24px;
+  text-align: right;  
+}
+#chart {
+  top: 100%;
+  left: 40%;
+  position: absolute;
+  margin: 4px;  
 }
 .clear-btn{
-  top: 75%;
+  top: 78%;
   height: 40px;
   width: 100px;
   right: 20%;
@@ -128,7 +159,7 @@ img {
   padding-top: 8px;
 }
 .course-content-step {
-  top: 46%;
+  top: 48%;
   right: 18%;
   position: absolute;
 }
