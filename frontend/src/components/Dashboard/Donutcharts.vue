41b78336ea7e406142255c489bc9b4bd372ec88b
이연지 <yeonji1012@naver.com>
2021-11-01 18:25:55 +0900

    S05P31C201-92 feat: 대시보드 운동종류별 운동량(도넛그래프)

---
+++ frontend/src/components/Dashboard/Donutcharts.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <apexchart width="500" height="350" type="donut" :options="chartOptions" :series="series"></apexchart>
+
+  </div>
+</template>
+
+<script>
+import VueApexCharts from 'vue-apexcharts'
+
+export default {
+  name: "Donutcharts",
+  components:{
+    apexchart: VueApexCharts,
+  },
+  data: function() {
+    return {
+      chartOptions: {
+        chart: { id: "basic-bar"},
+        xaxis: { categories: ["이번달 평균", "가장 최근(언제인지 적어야할거같은데..)",] }
+      },
+      series: [30, 12, 39, 20,]
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/common.scss";
+
+
+
+</style>
---
+++ frontend/src/components/Dashboard/Linecharts.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Linecharts",
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/common.scss";
+
+
+
+</style>
