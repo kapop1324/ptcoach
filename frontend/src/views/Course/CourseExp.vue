208dfe8786c4d85a1a2d4e598fcfab125f2e8593
심지영 <tlawldud96@naver.com>
2021-11-03 14:42:49 +0900

    [S05P31C201-81] feat: Update CourseExp.vue

--- frontend/src/views/Course/CourseExp.vue
+++ frontend/src/views/Course/CourseExp.vue
@@ -2,7 +2,8 @@
 <template>
   <div class="contents">
       <div class="title"><h1>{{exerciseList[step].exercise_name}}</h1></div>        
-      <div class="imageDiv"><img src='@/assets/squat_sample_remove.png' alt="스쿼트 샘플이미지"/></div>
+      <div class="imageDiv"><img src="exerciseList[step].exercise_image[1].path" alt="샘플이미지"/></div>
+      <!-- <div>{{this.$route.params}}</div> -->
       <div class="Desc">
         <p v-for="image in exerciseList[step].exercise_image" :key="image.idx">
           {{image.desc}}
@@ -10,7 +11,7 @@
       </div>
       <!-- step 개수에 따라서 component 만들기 -->
       <div class="course-content-step">
-        <Exe @click= 'changeStep' v-for="(exercise,i) in exerciseList" v-bind:num="i+1" v-bind:key="exercise.idx"></Exe>
+        <Seq @change-step= 'changeStep' v-for="(exercise,i) in exerciseList" :num="i+1" :selected="step+1" :key="exercise.idx"></Seq>
       </div>
       <!-- 시작 버튼 누르면 courseDetail페이지로 -->
       <div class="start-btn">
@@ -20,38 +21,37 @@
 </template>
 
 <script>
-import Exe from '@/components/Detail/Exe.vue'
+import Seq from '@/components/Detail/Seq.vue'
 
 export default {
   name: 'CourseExp',
   components:{
-    Exe: Exe,
+    Seq: Seq,
   },
   data(){
     return {
       step: 0,
+      // select: 1,
       src(){
       //return `http://i5c102.p.ssafy.io/api/feed/${this.feed.images[0].newname}`
       return this.exerciseList[this.step].exercise_image.path;
       },
       exerciseList: [{idx:1, course_name:'코스 1', step:'1', exercise_name:'lunge', 
                       exercise_image:[{idx:0, path:"http://k5c201.p.ssafy.io/image/thumbnail/lunge.png", step:"1", desc:"1. 어깨 넓이로 발을 벌리고 양 팔은 몸에 가볍게 붙인다."}, 
-                      {idx:1, path:"@/assets/squat_sample_remove.png", step:"2", desc:"2. 숨을 들이쉬면서 무릎을 굽혀 허리를 내린다."}]},
-      {idx:2, course_name:'코스 2', step:'2', exercise_name:'lunge', 
-                      exercise_image:[{idx:0, path:"", step:"1", desc:"1. 어깨 넓이로 발을 벌리고 양 팔은 몸에 가볍게 붙인다."}, 
-                      {idx:1, path:"", step:"2", desc:"2. 숨을 들이쉬면서 무릎을 굽혀 허리를 내린다."}]},
-      // desc: `1. 어깨 넓이로 발을 벌리고 양 팔은 몸에 가볍게 붙인다.
-      //       2. 숨을 들이쉬면서 무릎을 굽혀 허리를 내린다.
-      //       3. 숨을 뱉으며 원래 자세로 돌아온다.`
+                      {idx:1, path:"@/assets/lunge_tmp.png", step:"2", desc:"2. 숨을 들이쉬면서 무릎을 굽혀 허리를 내린다."}]},
+      {idx:2, course_name:'코스 1', step:'2', exercise_name:'squat', 
+                      exercise_image:[{idx:0, path:"@/assets/squat_sample_remove.png", step:"1", desc:"1. 발을 골반 너비만큼 벌리고 선다."}, 
+                      {idx:1, path:"@/assets/squat_sample_remove.png", step:"2", desc:"2. 오른쪽 다리를 크게 앞으로 내딛는다."}]},
       ],
     }
   },
   methods: {
     toCourse(){
-      this.$router.push('/course/1');
+      this.$router.push(`/course/${this.$route.params.id}`);
     },
-    changeStep(){
-      // this.step = i;
+    changeStep(i) {
+      this.step = i-1;
+      console.log(this.step);
     },
   },
   computed: {
@@ -95,7 +95,7 @@ h1 {
 }
 
 .start-btn{
-  top: 75%;
+  bottom: 5%;
   height: 40px;
   width: 120px;
   right: 12%;
