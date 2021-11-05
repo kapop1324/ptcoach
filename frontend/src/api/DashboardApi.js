80af57c3d18a6184fa8298a0e86331ca76d37c7c
이연지 <yeonji1012@naver.com>
2021-11-05 15:45:47 +0900

    S05P31C201-102 feat: 대시보드 백엔드 데이터 바인딩 수정 전

--- frontend/src/api/DashboardApi.js
+++ frontend/src/api/DashboardApi.js
@@ -2,7 +2,8 @@ import http from '@/util/http-common';
 
 const GetDashboard = (callback, errorCallback) => {
   // params: 유저 이메일로 변경할것
-  http.get('/dashboard/record', {params: {email: "rlaxogjs1234"}})
+  // http.get('/dashboard/record', {params: {email: "rlaxogjs1234"}})
+  http.get('/dashboard/record', {params: {email: "aihello"}})
   .then((res) => callback(res))
   .catch((err) => errorCallback(err));
 };
