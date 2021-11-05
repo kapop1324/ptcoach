abd2743b82c0ec5058e2e79660a0735e27e48135
김태헌 <rlaxogjs1234@naver.com>
2021-11-05 09:29:16 +0900

    hot fix : exercise domain

--- backend/src/main/java/com/pt/domain/Exercise.java
+++ backend/src/main/java/com/pt/domain/Exercise.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Builder;
@@ -20,7 +21,7 @@ import lombok.ToString;
 public class Exercise {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idx;
 	
 	private String name;
