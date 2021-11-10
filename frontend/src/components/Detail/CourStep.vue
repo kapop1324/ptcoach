00657dcfa36b07418b3b160945f6aec9f7152d72
박세영 <seyoungs22@naver.com>
2021-11-10 17:28:06 +0900

    [S05P31C201-119] : feat 캐러셀 추가, 운동 이름 버튼 구현

--- frontend/src/components/Detail/CourStep.vue
+++ frontend/src/components/Detail/CourStep.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="StepRoot">
       <div class="step-con">
-        <div v-if="num!=newstep" class="step-box">운동{{num}}</div>
-        <div v-else-if="num==newstep" class="new-step-box">운동{{num}}</div>
+        <div v-if="num!=newstep" class="step-box">{{name}}</div>
+        <div v-else-if="num==newstep" class="new-step-box">{{name}}</div>
       </div>
     </div>
 </template>
@@ -12,6 +12,7 @@ export default {
   name: "CourStep",
   props: {
     num: Number,
+    name: String,    
     newstep: Number,
   },
 }
@@ -21,28 +22,27 @@ export default {
 @import "@/styles/common.scss";
 .step-box {
   top: 16%;
-  height: 30px;
-  width: 120px;
+  height: 35px;
+  width: 140px;
   right: 6%;
   border-radius: 30px;
-  background-color: #968989;
   background-color: white;
-  font-size: 20px;
+  font-size: 18px;
   text-align: center;
   margin: 20px;
-  padding-top: 8px;
+  padding-top: 10px;
 }
 .new-step-box {
   top: 16%;
-  height: 30px;
-  width: 120px;
+  height: 35px;
+  width: 130px;
   right: 6%;
   border-radius: 30px;
   background-color: $light-color;
   font-size: 18px;
   text-align: center;
   margin: 20px;
-  padding-top: 8px;
+  padding-top: 10px;
   transform:scale(1.045);
   box-shadow: 0 0 10px #99a6ff21, 0 0 15px #99a6ff21, 0 0 25px #99a6ff21;
 }
