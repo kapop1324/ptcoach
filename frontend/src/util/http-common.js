213707d60a43f3fd064ecbb2c35137df27cb588e
류상오 <rko3507@naver.com>
2021-11-15 14:27:46 +0900

    [HotFix] fix https 수정

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // axios 객체 생성
 export default axios.create({
-  baseURL: 'https://k5c201.p.ssafy.io:8080/', //백엔드 포트번호
+  baseURL: 'https://k5c201.p.ssafy.io/api', //백엔드 포트번호
   headers: {
     'Content-type': 'application/json',
     'Access-Control-Allow-Origin' : '*',
