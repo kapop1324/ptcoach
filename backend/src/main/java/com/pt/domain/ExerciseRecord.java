160a8ce8ac6bd3f3d69ebd213c150231711a8fd4
류상오 <rko3507@naver.com>
2021-11-17 11:55:19 +0900

    [HotFix] fix 레코드 저장문제

--- backend/src/main/java/com/pt/domain/ExerciseRecord.java
+++ backend/src/main/java/com/pt/domain/ExerciseRecord.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -25,7 +26,7 @@ import lombok.ToString;
 public class ExerciseRecord {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int idx;
 	
 	@Column(name = "user_email")
